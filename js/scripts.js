a641b34789e7ef94ff05b0de56f94159110d725a
Alex Lee and Colin Bloom <alex@>
2017-02-14 16:20:47 -0800

    talking webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,36 @@
-$(function(){
-  $('li').click(function() {
-    $(this).children().toggle();
- });
+$(function() {
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Adios!</li>");
+      $("ul#user").children("li").click(function() {
+        $(this).remove();
+      });
+      $("ul#webpage").children("li").click(function() {
+        $(this).remove();
+      });
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>My fault! Won't happen again!</li>");
+      $("ul#user").children("li").click(function() {
+        $(this).remove();
+      });
+      $("ul#webpage").children("li").click(function() {
+        $(this).remove();
+      });
+  });
+
+
 });
